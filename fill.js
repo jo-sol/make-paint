62ac0cf24fbc1e1ab7ed1e242b6ce8580d01e7c6
jo-sol <zoixssm@gmail.com>
2021-12-01 01:05:32 +0900

    1201 push 투명도 학습하기(globalAlpha, rgba)

--- fill.js
+++ fill.js
@@ -42,6 +42,47 @@ function draw() {
 
     // --------------------------------------------------------------------------------
 
-    
+    // 투명도 조절하기 - globalAlpha 예제
 
+    // 배경을 그린다
+    // ctx.fillStyle = '#FD0';
+    // ctx.fillRect(0, 0, 75, 75);
+    // ctx.fillStyle = '#6C0';
+    // ctx.fillRect(75, 0, 75, 75);
+    // ctx.fillStyle = '#09F';
+    // ctx.fillRect(0, 75, 75, 75);
+    // ctx.fillStyle = '#F30';
+    // ctx.fillRect(75, 75, 75, 75);
+    // ctx.fillStyle = '#FFF';
+
+    // 투명값을 설정한다
+    //ctx.globalAlpha = 0.2;
+
+    // 반투명한 원을 그린다
+    // for (var i = 0; i < 7; i++){
+    //     ctx.beginPath();
+    //     ctx.arc(75, 75, 10 + 10 * i, 0, Math.PI * 2, true);
+    //     ctx.fill();
+    // }
+    
+    // 투명도 조절하기 - rgba() 예제
+
+    // 배경을 그린다
+  ctx.fillStyle = 'rgb(255,221,0)';
+  ctx.fillRect(0,0,150,37.5);
+  ctx.fillStyle = 'rgb(102,204,0)';
+  ctx.fillRect(0,37.5,150,37.5);
+  ctx.fillStyle = 'rgb(0,153,255)';
+  ctx.fillRect(0,75,150,37.5);
+  ctx.fillStyle = 'rgb(255,51,0)';
+  ctx.fillRect(0,112.5,150,37.5);
+
+  // 반투명한 사각형을 그린다
+  for (var i=0;i<10;i++){
+    ctx.fillStyle = 'rgba(255,255,255,'+(i+1)/10+')';
+    for (var j=0;j<4;j++){
+      ctx.fillRect(5+i*14,5+j*37.5,14,27.5)
+    }
   }
+  
+}
