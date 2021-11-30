cb22353fcaec08ce3e195d847312467175375886
jo-sol <zoixssm@gmail.com>
2021-12-01 00:57:13 +0900

    1201 push fillStyle 학습하기

---
+++ fill.js
@@ -0,0 +1,27 @@
+// 스타일과 색 적용하기
+
+function draw() {
+    var ctx = document.getElementById('canvas').getContext('2d');
+
+    // 그라데이션으로 색 적용하기
+    // for (var i = 0; i < 6; i++){
+    //   for (var j = 0; j < 6; j++){
+    //     ctx.fillStyle = 'rgb(' + Math.floor(255 - 42.5 * i) + ', ' +
+    //                      Math.floor(255 - 42.5 * j) + ', 0)';
+    //     ctx.fillRect(j*25,i*25,25,25);
+    //   }
+    // }
+
+    // fillStlye 예제 익히기
+    // ctx.fillStyle = "orange";
+    // ctx.fillStyle = "#FFA500";
+    // ctx.fillStyle = "rgb(255, 165, 0)";
+    // ctx.fillStyle = "rgba(255, 165, 0, 1)";
+    // ctx.fillRect(10, 10, 100, 100);
+
+    // --------------------------------------------------------------------------------
+
+    
+
+
+  }
