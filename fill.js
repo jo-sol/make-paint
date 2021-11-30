4130725043bb647c55d177ca014e0f82d41fad36
jo-sol <zoixssm@gmail.com>
2021-12-01 01:01:56 +0900

    1201 push strokeStyle 학습하기

--- fill.js
+++ fill.js
@@ -12,7 +12,7 @@ function draw() {
     //   }
     // }
 
-    // fillStlye 예제 익히기
+    // fillStlye 예제 익히기 - 직사각형
     // ctx.fillStyle = "orange";
     // ctx.fillStyle = "#FFA500";
     // ctx.fillStyle = "rgb(255, 165, 0)";
@@ -21,7 +21,27 @@ function draw() {
 
     // --------------------------------------------------------------------------------
 
-    
+    // strokeStyle 속성을 사용하여 윤곽선 색 바꾸기
+    // for (var i = 0; i < 6; i++) {
+    //     for (var j = 0; j < 6; j++) {
+    //         ctx.strokeStyle = 'rgb(0, ' + Math.floor(255 - 42.5 * i) + ', ' +
+    //                         Math.floor(255 - 42.5 * j) + ')';
+    //         ctx.beginPath();
+    //         ctx.arc(12.5 + j * 25, 12.5 + i * 25, 10, 0, Math.PI * 2, true);
+    //         ctx.stroke();
+    //     }
+    // }
 
+    // strokeStlye 예제 익히기 - 원
+    // ctx.strokeStyle = "blue";
+    // ctx.beginPath();
+    // ctx.arc(75, 75, 50, 0, Math.PI * 2, true);
+    // ctx.fillStyle = "yellow";
+    // ctx.stroke();
+    // ctx.fill();
+
+    // --------------------------------------------------------------------------------
+
+    
 
   }
